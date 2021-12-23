2d318a19ed3190ae4291fad18332e33c814ad089
bikramoli <bikramoli75@gmail.com>
2021-12-23 13:13:45 +0545

    navbar style

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
+import "./Navbar.css";
 
 import { ImBlog } from "react-icons/im";
 import {
