a7325fb1ea4be220b0dfc0b5c84752e42e6c14f7
bikramoli <bikramoli75@gmail.com>
2021-12-23 23:32:13 +0545

    adding animation using particle bg

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,36 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import  Typewriter from "typewriter-effect";
 import Introduction from "./Introduction";
+import ParticlesBg from 'particles-bg'
 
 import './Home.css'
 
 function Home() {
+  let config = {
+    num: [4, 7],
+    rps: 0.1,
+    radius: [5, 40],
+    life: [1.5, 3],
+    v: [2, 3],
+    tha: [-40, 40],
+    // body: "./img/icon.png", // Whether to render pictures
+    // rotate: [0, 20],
+    alpha: [0.6, 0],
+    scale: [1, 0.1],
+    position: "center", // all or center or {x:1,y:1,width:100,height:100}
+    color: ["random", "#ff0000"],
+    cross: "dead", // cross or bround
+    random: 15,  // or null,
+    g: 5,    // gravity
+    // f: [2, -1], // force
+    onParticleUpdate: (ctx, particle) => {
+        ctx.beginPath();
+        ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+        ctx.fillStyle = particle.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+  };
   return (
     
     <section>
@@ -38,6 +64,9 @@ function Home() {
                 }}
                 />
               </div>
+            </Col>
+            <Col md={5} style={{ paddingBottom: 20 }}>
+            <ParticlesBg type="custom" config={config} bg={true} />
             </Col>
           </Row>
         </Container>
