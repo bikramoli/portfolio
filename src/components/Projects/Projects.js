45b8c57a33779dca9c1e140ada53ada73f2c0412
bikramoli <bikramoli75@gmail.com>
2021-12-23 22:18:46 +0545

    project fetching from github and displaying in project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,89 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import Container from "react-bootstrap/Container";
 
-function Projects(){
+import Row from "react-bootstrap/Row";
+import ProjectCard from "./ProjectCard";
+import axios from "axios";
 
-    return (
-        <>
-        <h1>Projects Page</h1>
-        </>
-    )
-}
-export default Projects;
+const dummyProject = {
+  name: null,
+  description: null,
+  svn_url: null,
+  stargazers_count: null,
+  languages_url: null,
+  pushed_at: null,
+};
+const API = "https://api.github.com";
+// const gitHubQuery = "/repos?sort=updated&direction=desc";
+const repos = {
+    show: true,
+    heading: "Recent Projects",
+    username: "hashirshoaeb", //i.e."johnDoe12Gh"
+    length: 4,
+    specfic: [],
+  };
+
+const Project = ({ heading, username, length, specfic }) => {
+  const allReposAPI = `${API}/users/bikramoli/repos?sort=updated&direction=desc`;
+  const specficReposAPI = `${API}/repos/bikramoli`;
+  const dummyProjectsArr = new Array(repos.length + repos.specfic.length).fill(
+    dummyProject
+  );
+
+  const [projectsArray, setProjectsArray] = useState([]);
+
+  const fetchRepos = useCallback(async () => {
+    let repoList = [];
+    try {
+      // getting all repos
+      const response = await axios.get(allReposAPI);
+      // slicing to the length
+      repoList = [...response.data.slice(0, length)];
+      // adding specified repos
+      try {
+        for (let repoName of specfic) {
+          const response = await axios.get(`${specficReposAPI}/${repoName}`);
+          repoList.push(response.data);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+      // setting projectArray
+      // TODO: remove the duplication.
+      setProjectsArray(repoList);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }, [allReposAPI, length, specfic, specficReposAPI]);
+
+  useEffect(() => {
+    fetchRepos();
+  }, [fetchRepos]);
+
+  return (
+    <div fluid id="projects" className="bg-light m-0">
+      <Container className="">
+        <h2 className="display-4 pb-5 text-center">{repos.heading}</h2>
+        <Row>
+          {projectsArray.length
+            ? projectsArray.map((project, index) => (
+                <ProjectCard
+                  key={`project-card-${index}`}
+                  id={`project-card-${index}`}
+                  value={project}
+                />
+              ))
+            : dummyProjectsArr.map((project, index) => (
+                <ProjectCard
+                  key={`dummy-${index}`}
+                  id={`dummy-${index}`}
+                  value={project}
+                />
+              ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Project;
