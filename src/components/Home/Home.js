6320345fe535446f282af948e63214fab875a7ef
bikramoli <bikramoli75@gmail.com>
2021-12-29 23:30:39 +0545

    styling the serviece component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,9 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import  Typewriter from "typewriter-effect";
 import Introduction from "./Introduction";
-import drowsy from "../Assets/drowsy.jpg"
-import ParticlesBg from "particles-bg";
 import Particles from "../Particle/Particle";
+import Services from "../Services/Services";
 
 import './Home.css'
 
@@ -23,7 +22,7 @@ function Home() {
               </h1>
 
               <h1 className="heading-name">
-                I'M-
+                <strong >I'M-</strong>
                 <strong className="main-name">Developer</strong>
               </h1>
 
@@ -53,38 +52,8 @@ function Home() {
       <Container>
         
       </Container>
-      <div className='services'>
-        <div className='py-5'>
-        <h1 className='text-uppercase text-center py-4 pt-2'>What I do?</h1>
-            
-                <div className='container'>
-                    <div className='row'>
-                        <div className='col-md-4 col-sm-6'>
-                                {/* <FontAwesomeIcon icon={faPencilRuler} size='2x' className='icon__color'/> */}
-                                    <h3 className='pt-2 pb-2'>Web Design</h3>
-                                                    <p className='mb-4'>I can help with the first steps of creating your website with a wireframe or mock-up design. I can make a low-fidelity or high-fidelity mock-up that is professional and responsive</p>
-                            <hr className='break' />
-                            </div>
-                        
-                        {/* - */}
-                        <div className='col-md-4 col-sm-6'>
-                            {/* <FontAwesomeIcon icon={faCode}C size='2x' className='icon__color mt-2'/> */}
-                                    <h3 className='pt-2 pb-2'>Front End Development</h3>
-                                                    <p className='mb-4'>I will bring your website to life with the latest technologies! I can make a simple landing page, static or dynamic site, blog, or e-commerce site. I focus on mobile responsiveness, security, SEO, and accessibility.</p>
-                            <hr className='break' />
-                            </div>
-                            <div className='col-md-4 col-sm-6'>
-                            {/* <FontAwesomeIcon icon={faMobileAlt} size='2x' className='icon__color'/> */}
-                                    <h3 className='pt-2 pb-2'>Mobile Web Specialist</h3>
-                                                    <p className='mb-4'>Having a responsive web site is essentail! Half of all web traffic is from a mobile device! Having a responsive site can also affect your Google ranking! I can make sure your site is ready for the mobile world!</p>
-                    </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      <Services/>
       <Introduction/>
-      
-      
       
     </section>
   );
