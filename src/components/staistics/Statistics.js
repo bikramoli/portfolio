46d7266ceb22becadb3d277fd5c3f6ebfa7c6906
bikramoli <bikramoli75@gmail.com>
2022-02-02 01:30:38 +0545

    react countup to count when made visible

--- src/components/staistics/Statistics.js
+++ src/components/staistics/Statistics.js
@@ -2,6 +2,7 @@ import React, { memo } from "react";
 import { Container } from "react-bootstrap";
 import SectionTitle from "../section-title/SectionTitle";
 import VisibilitySensor from "react-visibility-sensor";
+import CountUp from "react-countup";
 import coverbikram2 from "../Assets/coverbikram2.jpg";
 import "./Statistics.css";
 
@@ -24,8 +25,28 @@ function Statistics() {
                   style={{ height: "150px" }}
                   src="https://cdn0.iconfinder.com/data/icons/round-ui-icons/512/tick_green.png"
                 />
-                <VisibilitySensor></VisibilitySensor>
-                <h1 className="mb-4 value">11</h1>
+                <VisibilitySensor>
+                  {({ isVisible }) => (
+                    <div>
+                      {isVisible ? (
+                        <CountUp
+                          className="mb-4 value"
+                          start={0}
+                          end={10}
+                          duration={1}
+                        ></CountUp>
+                      ) : (
+                        <CountUp
+                          className="mb-4 value"
+                          start={0}
+                          end={10}
+                          duration={2}
+                        ></CountUp>
+                      )}
+                    </div>
+                  )}
+                </VisibilitySensor>
+                {/* <h1 className="mb-4 value">11</h1> */}
                 <h3 className="pt-2 pb-2">Finished Projects</h3>
 
                 <hr className="break" />
